8af2cc1c558ceb2b68f89a4807d061277ee29a2f
dj5822 <dj5822@gmail.com>
2021-01-25 12:04:27 +1300

    Increment and decrement added

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -11,6 +11,36 @@ class Timer extends React.Component {
       sessionLength: 25
     };
     this.reset = this.reset.bind(this);
+    this.changeBreak = this.changeBreak.bind(this);
+    this.changeSession = this.changeSession.bind(this);
+  }
+
+  changeBreak(amount) {
+    let newBreakLength = 0;
+    if (this.state.breakLength + amount <= 0) {
+      newBreakLength = 1;
+    }
+    else {
+      newBreakLength = this.state.breakLength + amount;
+    }
+
+    this.setState({
+      breakLength: newBreakLength
+    })
+  }
+
+  changeSession(amount) {
+    let newSessionLength = 0;
+    if (this.state.sessionLength + amount <= 0) {
+      newSessionLength = 1;
+    }
+    else {
+      newSessionLength = this.state.sessionLength + amount;
+    }
+
+    this.setState({
+      sessionLength: newSessionLength
+    })
   }
 
   reset() {
@@ -23,8 +53,10 @@ class Timer extends React.Component {
   render() {
     return (
       <div>
-        <TimespanPanel type="break" length={this.state.breakLength} ></TimespanPanel>
-        <TimespanPanel type="session" length={this.state.sessionLength} ></TimespanPanel>
+        <TimespanPanel type="break" length={this.state.breakLength}
+        changeLength={this.changeBreak} />
+        <TimespanPanel type="session" length={this.state.sessionLength}
+        changeLength={this.changeSession} />
         <TimerPanel />
         <button id="start-stop">start/stop</button>
         <button id="reset" onClick={this.reset}>reset</button>
--- src/Components/TimespanPanel.js
+++ src/Components/TimespanPanel.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 class TimespanPanel extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.props.changeLength(1);
+  }
+
+  decrement() {
+    this.props.changeLength(-1);
+  }
+
   render() {
     return (
       <div>
         <label id={this.props.type+"-label"} >{this.props.type+" length"}</label>
-        <button id={this.props.type+"-decrement"}>+</button>
+        <button id={this.props.type+"-decrement"} onClick={this.increment} >+</button>
         <label id={this.props.type+"-length"}>{this.props.length}</label>
-        <button id={this.props.type+"-increment"}>-</button>
+        <button id={this.props.type+"-increment"} onClick={this.decrement} >-</button>
       </div>
     );
   }
