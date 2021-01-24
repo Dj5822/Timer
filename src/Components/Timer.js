83f0a9863b9caa7dcbe3525aab1db45c823b517d
dj5822 <dj5822@gmail.com>
2021-01-25 11:46:53 +1300

    Implement reset functionality

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -10,6 +10,14 @@ class Timer extends React.Component {
       breakLength: 5,
       sessionLength: 25
     };
+    this.reset = this.reset.bind(this);
+  }
+
+  reset() {
+    this.setState({
+      breakLength: 5,
+      sessionLength: 25
+    });
   }
 
   render() {
@@ -19,7 +27,7 @@ class Timer extends React.Component {
         <TimespanPanel type="session" length={this.state.sessionLength} ></TimespanPanel>
         <TimerPanel />
         <button id="start-stop">start/stop</button>
-        <button id="reset">reset</button>
+        <button id="reset" onClick={this.reset}>reset</button>
       </div>
     );
   }
