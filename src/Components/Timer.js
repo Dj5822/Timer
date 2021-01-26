bb3445f792a5f4a51c2527f26b55acca36b55231
dj5822 <dj5822@gmail.com>
2021-01-26 15:14:19 +1300

    timer status switches to break.
    Timer status switches to break once the session is over.

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -3,6 +3,7 @@ import TimespanPanel from './TimespanPanel.js';
 import TimerPanel from './TimerPanel.js';
 
 var countdown;
+const STATUS = ["session", "break"];
 
 class Timer extends React.Component {
   constructor(props) {
@@ -13,7 +14,8 @@ class Timer extends React.Component {
       sessionLength: 25,
       min: 25,
       sec: 0,
-      countdownStarted: false
+      countdownStarted: false,
+      status: STATUS[0]
     };
     this.reset = this.reset.bind(this);
     this.changeBreak = this.changeBreak.bind(this);
@@ -64,7 +66,8 @@ class Timer extends React.Component {
       sessionLength: 25,
       min: 25,
       sec: 0,
-      countdownStarted: false
+      countdownStarted: false,
+      status: STATUS[0]
     });
   }
 
@@ -80,6 +83,27 @@ class Timer extends React.Component {
         min: timeLeft.getMinutes(),
         sec: timeLeft.getSeconds()
       }));
+
+      /*
+        Reset timer.
+      */
+      if (timeLeft.getMinutes() === 0 && timeLeft.getSeconds() === 0){
+        if (this.state.status === STATUS[0]){
+          this.setState(state => ({
+            min: state.breakLength,
+            status: STATUS[1]
+          }));
+          session = new Date(this.state.breakLength*60*1000);
+        }
+        else {
+          this.setState(state => ({
+            min: state.sessionLength,
+            status: STATUS[0]
+          }));
+          session = new Date(this.state.sessionLength*60*1000);
+        }
+        startTime = new Date();
+      }
     }.bind(this),
     500);
   }
@@ -108,7 +132,7 @@ class Timer extends React.Component {
         changeLength={this.changeBreak} />
         <TimespanPanel type="session" length={this.state.sessionLength}
         changeLength={this.changeSession} />
-        <TimerPanel minutes={this.state.min} seconds={this.state.sec} />
+        <TimerPanel status={this.state.status} minutes={this.state.min} seconds={this.state.sec} />
         <button id="start-stop" onClick={this.startStopPressed}>start/stop</button>
         <button id="reset" onClick={this.reset}>reset</button>
       </div>
--- src/Components/TimerPanel.js
+++ src/Components/TimerPanel.js
@@ -4,7 +4,7 @@ class TimerPanel extends React.Component {
   render() {
     return (
       <div>
-        <label id="timer-label">Session</label>
+        <label id="timer-label">{this.props.status}</label>
         <label id="time-left">
         {this.props.minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}
         :{this.props.seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}
